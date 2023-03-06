34088c904208f9710513991d9b18fef6474d357f
ThomasJohnen <thomas.johnen@hotmail.be>
2023-03-06 11:19:40 +0100

    adding : (src) adding Graph class

---
+++ src/Graph.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class Graph {
+
+  private File lignes, troncons;
+  
+  public Graph(File lignes, File troncons) {
+    this.lignes = lignes;
+    this.troncons = troncons;
+  }
+
+  public void calculerCheminMinimisantNombreTroncons(String arret, String arret2) {
+  }
+
+  public void calculerCheminMinimisantTempsTransport(String arret, String arret2) {
+  }
+}
